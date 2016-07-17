0fe531d9357c0271191a7e3cce0b484212b04079
Kyl Wellman <kyl@gratafy.com>
2016-07-17 13:17:59 -0700

    tests I willprobably never use

---
+++ src/browser/commish/tests/unit/helpers/get-attr-test.js
@@ -0,0 +1,10 @@
+import { getAttr } from 'commish/helpers/get-attr';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get attr');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getAttr([42]);
+  assert.ok(result);
+});
---
+++ src/browser/commish/tests/unit/helpers/start-case.js-test.js
@@ -0,0 +1,10 @@
+import { startCaseJs } from 'commish/helpers/start-case.js';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | start case.js');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = startCaseJs([42]);
+  assert.ok(result);
+});
