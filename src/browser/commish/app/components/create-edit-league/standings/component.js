0d870855bfcb6a89d90eb477c749c51235248907
Kyl Wellman <kyl@gratafy.com>
2016-07-17 22:37:55 -0700

    start working on score entry

--- src/browser/commish/app/components/create-edit-league/standings/component.js
+++ src/browser/commish/app/components/create-edit-league/standings/component.js
@@ -3,9 +3,9 @@ import ResourceList from 'commish/components/resource-list/component';
 
 export default ResourceList.extend({
   standings: Ember.inject.service(),
+  enterResultState: 'closed',
   init() { 
     this._super(...arguments);
-    this.set('rows', this.get('leagues'));
     this.get('standings').loadLeagueTable(this.get('league')).then(
       (table) => {
         this.set('leagueTable', table);
@@ -18,5 +18,42 @@ export default ResourceList.extend({
         this.set('leagueTable', table);
       }
     );
-  })
+  }),
+  resultTeamsChanged: Ember.observer('enterResultHomeTeam', 'enterResultAwayTeam', function() {
+    let home = this.get('enterResultHomeTeam');
+    let away = this.get('enterResultAwayTeam');
+
+    if (!home || !away) {
+      return;
+    }
+
+    if (home.get('id') === away.get('id')) {
+      home.set('isError', true);
+      away.set('isError', true);
+      return;
+    }
+
+    home.set('isError', false);
+    away.set('isError', false);
+    this.set('enterResultState', 'enter-scores');
+  }),
+  actions: {
+    setEnterResultState (state) {
+      this.set('enterResultState', state);
+
+      if (state === 'closed') {
+        // Clear state
+      }
+    },
+    selectHomeTeam (team) {
+      this.get('league.teams').forEach ( (t) => t.set('isHomeSelected', false));
+      team.set('isHomeSelected', true);
+      this.set('enterResultHomeTeam', team);
+    },
+    selectAwayTeam (team) {
+      this.get('league.teams').forEach ( (t) => t.set('isAwaySelected', false));
+      team.set('isAwaySelected', true);
+      this.set('enterResultAwayTeam', team);
+    }
+  }
 });
